442564e360e5ca40400289d287b02171de72f216
chriscauley <chris@lablackey.com>
2019-02-05 03:27:46 +0000

    importing unrest urls

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 
-urlpatterns = [path("admin/", admin.site.urls)]
+import unrest.urls
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    re_path('', include(unrest.urls)),
+]
